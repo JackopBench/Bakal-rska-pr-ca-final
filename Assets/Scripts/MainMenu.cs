aa66906c97bc6f81acdd5ad8e05cf4f9373fea37
Jakub Lavička <161425223+JackopBench@users.noreply.github.com>
2026-04-17 15:45:01 +0200

    Tutorial Button added - Final commit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,6 +15,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(gameSceneName);
     }
 
+    public void PlayTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
